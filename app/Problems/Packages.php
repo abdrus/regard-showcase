d588d1121bfc2681f91d78f1ce183630b6815ab0
Ruslan Abdullaev <Ruslan.Abdullaev@softline.com>
2024-11-15 00:36:28 +0500

    refs #8 Codestyle autofixes

--- app/Problems/Packages.php
+++ app/Problems/Packages.php
@@ -2,37 +2,28 @@
 
 namespace App\Problems;
 
-use Doctrine\Inflector\Rules\Word;
-
-class Packages {
-
+class Packages
+{
     /**
-    * @param array $boxes
-    * @param int $weight
-    * @return int | null
-    */
+     * @return int | null
+     */
     public function solve(array $boxes, int $weight): int
     {
         $travels = 0;
         $pairs = [];
 
-        if (empty($boxes) || empty($weight))
-        {
+        if (empty($boxes) || empty($weight)) {
             return 0;
         }
 
-        foreach ($boxes as $currentIndex => $currentBox)
-        {
-            for ($i = 0; $i < count($boxes); $i++)
-            {
+        foreach ($boxes as $currentIndex => $currentBox) {
+            for ($i = 0; $i < count($boxes); $i++) {
                 if ($currentIndex == $i) {
                     continue;
                 }
 
-                if ($currentBox + $boxes[$i] == $weight)
-                {
-                    if (!$this->hasPairAlready($pairs, $currentIndex, $i))
-                    {
+                if ($currentBox + $boxes[$i] == $weight) {
+                    if (! $this->hasPairAlready($pairs, $currentIndex, $i)) {
                         // Для дебага, если интересно
                         // printf("%d(%d) + %d(%d) = %d\n", $currentBox, $currentIndex, $boxes[$i], $i, $weight);
                         $travels++;
@@ -46,19 +37,14 @@ class Packages {
     }
 
     /**
-    * @param array $pairs - массив пар индексов
-    * @param int $index1
-    * @param int $index2
-    * @return bool
-    */
+     * @param  array  $pairs  - массив пар индексов
+     */
     public function hasPairAlready(array $pairs, int $index1, int $index2): bool
     {
-        if (in_array([$index1, $index2], $pairs) or in_array([$index2, $index1], $pairs))
-        {
+        if (in_array([$index1, $index2], $pairs) or in_array([$index2, $index1], $pairs)) {
             return true;
         }
 
         return false;
     }
-
 }
--- tests/Unit/Problems/PackagesTest.php
+++ tests/Unit/Problems/PackagesTest.php
@@ -2,55 +2,54 @@
 
 namespace Tests\Unit\Problems;
 
-use Tests\TestCase;
 use App\Problems\Packages;
+use Tests\TestCase;
 
-class PackagesTest extends TestCase {
-
+class PackagesTest extends TestCase
+{
     /**
-    * @test
-    */
+     * @test
+     */
     public function ItTestsPackages(): void
     {
-       $task = new Packages();
+        $task = new Packages;
 
-       $boxes = [2,4,3,6,1];
-       $weight = 5;
+        $boxes = [2, 4, 3, 6, 1];
+        $weight = 5;
 
-       $this->assertEquals(2, $task->solve($boxes, $weight));
+        $this->assertEquals(2, $task->solve($boxes, $weight));
 
-       $boxes = [1, 2, 1, 5, 1, 3, 5, 2, 5, 5];
-       $weight = 6;
+        $boxes = [1, 2, 1, 5, 1, 3, 5, 2, 5, 5];
+        $weight = 6;
 
-       $this->assertEquals(12, $task->solve($boxes, $weight));
+        $this->assertEquals(12, $task->solve($boxes, $weight));
 
     }
 
     /**
-    * @test
-    */
+     * @test
+     */
     public function ItTestsEmptyPackages(): void
     {
-       $task = new Packages();
+        $task = new Packages;
 
-       $boxes = [];
-       $weight = 0;
+        $boxes = [];
+        $weight = 0;
 
-       $this->assertEquals(0, $task->solve($boxes, $weight));
+        $this->assertEquals(0, $task->solve($boxes, $weight));
     }
 
     /**
-    * @test
-    */
+     * @test
+     */
     public function ItTestsPairs(): void
     {
-        $task = new Packages();
-        $this->assertTrue($task->hasPairAlready([[1,2]], 1, 2));
-        $this->assertTrue($task->hasPairAlready([[1,2]], 2, 1));
-        $this->assertTrue($task->hasPairAlready([[1,2], [3,4]], 2, 1));
-        $this->assertTrue($task->hasPairAlready([[1,2], [3,4]], 4, 3));
+        $task = new Packages;
+        $this->assertTrue($task->hasPairAlready([[1, 2]], 1, 2));
+        $this->assertTrue($task->hasPairAlready([[1, 2]], 2, 1));
+        $this->assertTrue($task->hasPairAlready([[1, 2], [3, 4]], 2, 1));
+        $this->assertTrue($task->hasPairAlready([[1, 2], [3, 4]], 4, 3));
 
-        $this->assertFalse($task->hasPairAlready([[1,2], [3,4]], 5, 6));
+        $this->assertFalse($task->hasPairAlready([[1, 2], [3, 4]], 5, 6));
     }
-
 }
